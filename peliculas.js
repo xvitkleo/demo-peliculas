e7fccb7c224cc38cd483216eda09474f29e0cadd
Leonardo <58629843+xvitkleo@users.noreply.github.com>
2026-09-21 16:48:32 -0500

    Create peliculas.js to serve movie data via API

---
+++ peliculas.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const { DatabaseSync } = require('node:sqlite');
+const db = new DatabaseSync('./peliculas.db')
+
+const server = http.createServer((req, res) => {
+ const peliculas = db.prepare('SELECT * FROM peliculas').all();
+
+ res.writeHead(200, {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  });
+
+ res.end(JSON.stringify(peliculas));
+});
+server.listen(3000, '0.0.0.0', () => {
+  console.log('API escuchando en puerto 3000');
+});;
