845c5930dbd79712772fa984a648f10cb4c21466
Leonardo <58629843+xvitkleo@users.noreply.github.com>
2026-09-21 19:24:35 -0500

    Rename column 'titulo' to 'nombre' in peliculas table

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { DatabaseSync } = require('node:sqlite');
 const db = new DatabaseSync('./peliculas.db')
 
 const server = http.createServer((req, res) => {
- db.exec(CREATE TABLE IF NOT EXISTS peliculas ( id INTEGER PRIMARY KEY, titulo TEXT ));
+ db.exec('CREATE TABLE IF NOT EXISTS peliculas ( id INTEGER PRIMARY KEY, nombre TEXT )');
  const peliculas = db.prepare('SELECT * FROM peliculas').all();
 
  res.writeHead(200, {
