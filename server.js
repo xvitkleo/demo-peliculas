fe5d132ea368d91ca5c926773d5c2d0a1129b549
Leonardo <58629843+xvitkleo@users.noreply.github.com>
2026-09-21 20:07:02 -0500

    Switch from SQLite to MSSQL for database operations
    Refactor database connection to use MSSQL and update query handling.

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const http = require('http');
-const { DatabaseSync } = require('node:sqlite');
-const db = new DatabaseSync('./peliculas.db')
+sql = require('mssql');
+const config = { 
+ server: process.env.DB_SERVER,
+ database: process.env.DB_DATABASE,
+ user: process.env.DB_USER,
+ password: process.env.DB_PASSWORD,
+ options: { encrypt: true, trustServerCertificate: false } 
+};
 
+const pool = await sql.connect(config);
 const server = http.createServer((req, res) => {
- db.exec('CREATE TABLE IF NOT EXISTS peliculas ( id INTEGER PRIMARY KEY, nombre TEXT )');
- const peliculas = db.prepare('SELECT * FROM peliculas').all();
+ const peliculas = await pool.request().query( 'SELECT * FROM peliculas' );
 
  res.writeHead(200, {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*'
   });
 
- res.end(JSON.stringify(peliculas));
+ res.end(JSON.stringify(peliculas.recordset));
 });
 server.listen(process.env.PORT || 3000, '0.0.0.0', () => {
   console.log('API escuchando en puerto 3000');
